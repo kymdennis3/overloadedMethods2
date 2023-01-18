5fe9cc8e3826a75c8c1c46320b093869d7c2948d
Kimtai <kymdennis3@gmail.com>
2023-01-18 05:53:12 +0300

    using double

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         //                      method name + parameters = methods signature
 
         int z = add(7,19);
-        System.out.println(z);
+        double y = add(8.5, 9.0);
+        System.out.println(y);
 
     }
     static int add(int a, int b){
@@ -18,6 +19,18 @@ public class Main {
     }
     static int add(int a, int b, int c, int d){
         System.out.println("This overloaded method #3");
+        return a + b + c + d;
+    }
+    static double add(double a, double b){
+        System.out.println("This overloaded method #4");
+        return a + b;
+    }
+    static double add(double a, double b, double c){
+        System.out.println("This overloaded method #5");
+        return a + b + c;
+    }
+    static double add(double a, double b, double c, double d){
+        System.out.println("This overloaded method #6");
         return a + b + c + d;
     }
 }
