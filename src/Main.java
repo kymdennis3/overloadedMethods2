a18b572e743ecf21e618a1dc2ecf0b541eafc10a
Kimtai <kymdennis3@gmail.com>
2023-01-18 05:45:45 +0300

    This overloaded method 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // overloaded methods = methods that share the same name but have different parameters
+        //                      method name + parameters = methods signature
+
+        int z = add(7,19, 4);
+        System.out.println(z);
+
+    }
+    static int add(int a, int b){
+        System.out.println("This overloaded method #1");
+        return a + b;
+    }
+    static int add(int a, int b, int c){
+        System.out.println("This overloaded method #2");
+        return a + b + c;
+    }
+    static int add(int a, int b, int c, int d){
+        System.out.println("This overloaded method #3");
+        return a + b + c + d;
     }
 }
